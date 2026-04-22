7e4526c90ac6efdbb2da5228be9bc1f60c6187bc
YitengZhang <yiteng@umich.edu>
2026-04-21 17:01:14 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Hello!! I am Yiteng Zhang (yiteng) <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+        Count times 3: {count * 3}
+      </section>
+
+      <div className="ticks"></div>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
